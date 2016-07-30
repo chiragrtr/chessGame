92082e22bafb0424112380b3a3b4188f0c1bdae4
Himansh2294 <himanshu00238@gmail.com>
2016-07-30 13:02:39 +0530

    updated the file, included method - getSquare

--- Board.java
+++ Board.java
@@ -1,6 +1,21 @@
 /**
- * Created by palsulea on 7/30/2016.
+ * Created by guptah on 7/30/2016.
  */
 public class Board {
+    private Square[][] squares = new Square[8][8];
+
+    public Board() {
+        for(int i=0; i<squares.length; i++){
+            for(int j=0; j<squares.length; j++){
+                this.squares[i][j] = new Square(i, j);
+            }
+        }
+    }
+
+    public Square getSquare(int x, int y) {
+        return squares[x][y];
+    }
+
+}
 
 }
