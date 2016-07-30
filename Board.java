ea4790ab759fddc80753c47508173f2334c8497e
Himansh2294 <himanshu00238@gmail.com>
2016-07-30 14:20:35 +0530

    initializing the pieces

--- Board.java
+++ Board.java
@@ -3,6 +3,7 @@
  */
 public class Board {
     private ChessMan[][] chessManBoard = new chessManBoard[8][8];
+     private List<Piece> pieces = new ArrayList<>();
 
     public Board() {
         for(int rank=0; i<chessManBoard.length; rank++){
@@ -14,6 +15,36 @@ public class Board {
 
     public Square getChessMan(int rank, int file) {
         return chessManBoard[rank][file];
+    }
+    
+     public void initializePieces(){
+        if(this.white == true){
+            for(int i=0; i<PAWNS; i++){
+                pieces.add(new Pawn(true,i,2));
+            }
+            pieces.add(new Rook(true, 0, 0));
+            pieces.add(new Rook(true, 7, 0));
+            pieces.add(new Bishop(true, 2, 0));
+            pieces.add(new Bishop(true, 5, 0));
+            pieces.add(new Knight(true, 1, 0));
+            pieces.add(new Knight(true, 6, 0));
+            pieces.add(new Queen(true, 3, 0));
+            pieces.add(new King(true, 4, 0));
+        }
+        else{
+            for(int i=0; i<PAWNS; i++){ // draw pawns
+                pieces.add(new Pawn(true,i,6));
+            }
+            pieces.add(new Rook(true, 0, 7));
+            pieces.add(new Rook(true, 7, 7));
+            pieces.add(new Bishop(true, 2, 7));
+            pieces.add(new Bishop(true, 5, 7));
+            pieces.add(new Knight(true, 1, 7));
+            pieces.add(new Knight(true, 6, 7));
+            pieces.add(new Queen(true, 3, 7));
+            pieces.add(new King(true, 4, 7));
+        }
+
     }
 
 }
