685a9d1077f862737b2b95cb60d2be22e3481c8b
AbhishekPalsule <abhishek.palsule@gmail.com>
2016-07-30 12:17:34 +0530

    Add files via upload

--- Pawn.java
+++ Pawn.java
@@ -1,18 +1,37 @@
 /**
- * Created by sharmach on 7/30/2016.
+ * Created by palsulea on 7/30/2016.
  */
 public class Pawn extends ChessMan {
+
+
+
+    public Pawn(int rank,int file){
+        this.rank = rank;
+        this.file = file;
+    }
+
+    public int getRank(){
+     return this.rank;
+    }
+
+    public int getFile(){
+        return super.file;
+    }
     @Override
     public void movement() {
 
     }
+
+
     public void capture() {
 
     }
+
     public void promote(){
 
     }
     public void enPassant(){
 
     }
+
 }
--- Rook.java
+++ Rook.java
@@ -1,7 +1,13 @@
 /**
- * Created by sharmach on 7/30/2016.
+ * Created by palsulea on 7/30/2016.
  */
 public class Rook extends Piece {
+
+    public Rook(int rank,int file) {
+        this.rank = rank;
+        this.file = file;
+    }
+
     @Override
     public void movement() {
 
